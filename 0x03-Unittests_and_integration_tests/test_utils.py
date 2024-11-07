f76750398a371177f1fe00a86518d16ba474eb08
yohannes4321 <alemuyohannes960@gmail.com>
2024-11-07 09:14:18 +0300

    patch

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,21 +1,25 @@
 #!/usr/bin/env python3
-
 import unittest
+from unittest.mock import Mock, patch
+import requests
 from parameterized import parameterized
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 
 
-class TestAccessNestedMap(unittest.TestCase):
-
+class TestGetJson(unittest.TestCase):
     @parameterized.expand([
-        ({}, ("a",), 'a'),
-        ({"a": 1}, ("a", "b"), 'b')
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
     ])
-    def test_access_nested_map_exception(self, nested_map, path, expected):
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
-        # The exception message includes quotes around the key
-        self.assertEqual(str(context.exception), f"'{expected}'")
+    def test_get_json(self, test_url, test_payload):
+        with patch('requests.get') as mock_get:
+            mock = Mock()
+            mock.json.return_value = test_payload
+            mock_get.return_value = mock
+
+            result = get_json(test_url)
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
 
 
 if __name__ == "__main__":
